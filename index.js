ae26ee1e943923d8423b24ffbccc36d956cab4e2
Jordan Choo <jordan.choo@kogneta.com>
2022-01-25 12:45:05 -0500

    Added initial security checks

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const gServiceAccount = JSON.parse(process.env.gServiceAccount);
 
 exports.biggieCalls = (req, res) => {
     try {
-        
+        // Check to see if it is a POST Req
+        if(req.method != 'POST') return res.status(401).send('Not authorized');
+        // Make sure that the kgKey matches the env kgKey
+        if(req.query.kgKey != kgKey) return res.status(401).send('Not authorized');
     } catch (e) {
         
     }
