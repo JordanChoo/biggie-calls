d71adafec31c168e25334ba61000a930f2c1a591
Jordan Choo <jordan.choo@kogneta.com>
2022-01-25 12:42:28 -0500

    Added initial BQ ENV vars

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// Set ENV variables
+const bqDataset = process.env.dataset || null;
+const bgBiggieCallsTable = process.env.bgBiggieCallsTable || null;
+const bqProjectId = process.env.bqProjectId || null;
+const kgKey = process.env.kgKey || null;
+const gServiceAccount = JSON.parse(process.env.gServiceAccount);
+
 exports.biggieCalls = (req, res) => {
     try {
         
