9a300e6424e060259ef6a14d31d113475dc98b73
Jordan Choo <jordan.choo@kogneta.com>
2022-02-02 15:08:12 -0500

    Fixed indenting...

--- index.js
+++ index.js
@@ -117,125 +117,125 @@ module.exports = {
 
     squad: async(req,res) => {
         try {
-        // Check to see if it is a GET Req
-        if(req.method != 'GET') return res.status(401).send('Not authorized');
-        // Make sure that the kgKey matches the env kgKey
-        if(req.query.kgKey != kgKey) return res.status(401).send('Not authorized');
+            // Check to see if it is a GET Req
+            if(req.method != 'GET') return res.status(401).send('Not authorized');
+            // Make sure that the kgKey matches the env kgKey
+            if(req.query.kgKey != kgKey) return res.status(401).send('Not authorized');
 
-        var allCalls = [];
+            var allCalls = [];
 
-        // Axios get all all calls from call rail
-            // Run recursively until all calls are returned
-            while(true){
-                let page = 1;
-                var options = {
-                    'method': 'GET',
-                    'url': `https://api.callrail.com/v3/a/${callRailAccountId}/calls.json?fields=call_type,company_id,company_name,company_time_zone,created_at,device_type,first_call,formatted_call_type,formatted_customer_location,formatted_business_phone_number,formatted_customer_name,prior_calls,formatted_customer_name_or_phone_number,formatted_customer_phone_number,formatted_duration,formatted_tracking_phone_number,formatted_tracking_source,formatted_value,good_lead_call_id,good_lead_call_time,lead_status,note,source,source_name,total_calls,value,tracker_id,keywords,medium,campaign,referring_url,landing_page_url,last_requested_url,referrer_domain,utm_source,utm_medium,utm_term,utm_content,utm_campaign,utma,utmb,utmc,utmv,utmz,ga,gclid,fbclid,msclkid,keywords_spotted,agent_email&date_range=all_time&per_page=250&page=${page}`,
-                    'headers': {
-                        'Authorization': `Bearer ${callRailApiToken}`,
-                        'Content-Type': 'application/json'
-                    }
+            // Axios get all all calls from call rail
+                // Run recursively until all calls are returned
+                while(true){
+                    let page = 1;
+                    var options = {
+                        'method': 'GET',
+                        'url': `https://api.callrail.com/v3/a/${callRailAccountId}/calls.json?fields=call_type,company_id,company_name,company_time_zone,created_at,device_type,first_call,formatted_call_type,formatted_customer_location,formatted_business_phone_number,formatted_customer_name,prior_calls,formatted_customer_name_or_phone_number,formatted_customer_phone_number,formatted_duration,formatted_tracking_phone_number,formatted_tracking_source,formatted_value,good_lead_call_id,good_lead_call_time,lead_status,note,source,source_name,total_calls,value,tracker_id,keywords,medium,campaign,referring_url,landing_page_url,last_requested_url,referrer_domain,utm_source,utm_medium,utm_term,utm_content,utm_campaign,utma,utmb,utmc,utmv,utmz,ga,gclid,fbclid,msclkid,keywords_spotted,agent_email&date_range=all_time&per_page=250&page=${page}`,
+                        'headers': {
+                            'Authorization': `Bearer ${callRailApiToken}`,
+                            'Content-Type': 'application/json'
+                        }
+                    };
+                    
+                    // Run the Axios function
+                    let results = await axios.request(options);
+
+                    // Format and push data to allCalls array
+                    for(call of results.data.calls){
+                        allCalls.push({
+                            agent_email: call.agent_email,
+                            answered: call.answered,
+                            business_phone_number: call.business_phone_number,
+                            call_type: call.call_type,
+                            campaign: call.campaign,
+                            company_id: call.company_id,
+                            company_name: call.company_name,
+                            company_id: call.company_resource_id,
+                            company_time_zone: call.company_time_zone,
+                            conversational_transcript: call.conversational_transcript,
+                            created_at: call.created_at.slice(0,-6),
+                            customer_city: call.customer_city,
+                            customer_country: call.customer_country,
+                            customer_name: call.customer_name,
+                            customer_phone_number: call.customer_phone_number,
+                            customer_state: call.customer_state,
+                            device_type: call.device_type,
+                            direction: call.direction,
+                            duration: call.duration,
+                            fbclid: call.fbclid,
+                            first_call: call.first_call,
+                            formatted_business_phone_number: call.formatted_business_phone_number,
+                            formatted_call_type: call.formatted_call_type,
+                            formatted_customer_location: call.formatted_customer_location,
+                            formatted_customer_name: call.formatted_customer_name,
+                            formatted_customer_name_or_phone_number: call.formatted_customer_name_or_phone_number,
+                            formatted_customer_phone_number: call.formatted_customer_phone_number,
+                            formatted_duration: call.formatted_duration,
+                            formatted_tracking_phone_number: call.formatted_tracking_phone_number,
+                            formatted_tracking_source: call.formatted_tracking_source,
+                            formatted_value: call.formatted_value,
+                            ga: call.ga,
+                            gclid: call.gclid,
+                            good_lead_call_id: call.good_lead_call_id,
+                            good_lead_call_time: call.good_lead_call_time,
+                            keywords: call.keywords,
+                            landing_page_url: call.landing_page_url,
+                            last_requested_url: call.last_requested_url,
+                            lead_status: call.lead_status,
+                            medium: call.medium,
+                            msclkid: call.msclkid,
+                            note: call.note,
+                            prior_calls: call.prior_calls,
+                            recording: call.recording,
+                            recording_duration: call.recording_duration,
+                            recording_player: call.recording_player,
+                            referrer_domain: call.referrer_domain,
+                            referring_url: call.referring_url,
+                            resource_id: call.id,
+                            source: call.source,
+                            source_name: call.source_name,
+                            start_time: call.start_time.slice(0,-6),
+                            total_calls: call.total_calls,
+                            tracker_resource_id: call.tracker_id,
+                            tracking_phone_number: call.tracking_phone_number,
+                            transcription: call.transcription,
+                            utm_campaign: call.utm_campaign,
+                            utm_content: call.utm_content,
+                            utm_medium: call.utm_medium,
+                            utm_source: call.utm_source,
+                            utm_term: call.utm_term,
+                            utma: call.utma,
+                            utmb: call.utmb,
+                            utmc: call.utmc,
+                            utmv: call.utmv,
+                            utmz: call.utmz,
+                            value: call.value,
+                            voicemail: call.voicemail,
+                            raw_webhook: JSON.stringify(call)
+                        });
+                    };
+
+                    // End the loop if it is the last page
+                    if(results.data.calls.length < 250){
+                        break;
+                    };
+
+                    // Increment the page counter
+                    page++;
                 };
-                
-                // Run the Axios function
-                let results = await axios.request(options);
-
-                // Format and push data to allCalls array
-                for(call of results.data.calls){
-                    allCalls.push({
-                        agent_email: call.agent_email,
-                        answered: call.answered,
-                        business_phone_number: call.business_phone_number,
-                        call_type: call.call_type,
-                        campaign: call.campaign,
-                        company_id: call.company_id,
-                        company_name: call.company_name,
-                        company_id: call.company_resource_id,
-                        company_time_zone: call.company_time_zone,
-                        conversational_transcript: call.conversational_transcript,
-                        created_at: call.created_at.slice(0,-6),
-                        customer_city: call.customer_city,
-                        customer_country: call.customer_country,
-                        customer_name: call.customer_name,
-                        customer_phone_number: call.customer_phone_number,
-                        customer_state: call.customer_state,
-                        device_type: call.device_type,
-                        direction: call.direction,
-                        duration: call.duration,
-                        fbclid: call.fbclid,
-                        first_call: call.first_call,
-                        formatted_business_phone_number: call.formatted_business_phone_number,
-                        formatted_call_type: call.formatted_call_type,
-                        formatted_customer_location: call.formatted_customer_location,
-                        formatted_customer_name: call.formatted_customer_name,
-                        formatted_customer_name_or_phone_number: call.formatted_customer_name_or_phone_number,
-                        formatted_customer_phone_number: call.formatted_customer_phone_number,
-                        formatted_duration: call.formatted_duration,
-                        formatted_tracking_phone_number: call.formatted_tracking_phone_number,
-                        formatted_tracking_source: call.formatted_tracking_source,
-                        formatted_value: call.formatted_value,
-                        ga: call.ga,
-                        gclid: call.gclid,
-                        good_lead_call_id: call.good_lead_call_id,
-                        good_lead_call_time: call.good_lead_call_time,
-                        keywords: call.keywords,
-                        landing_page_url: call.landing_page_url,
-                        last_requested_url: call.last_requested_url,
-                        lead_status: call.lead_status,
-                        medium: call.medium,
-                        msclkid: call.msclkid,
-                        note: call.note,
-                        prior_calls: call.prior_calls,
-                        recording: call.recording,
-                        recording_duration: call.recording_duration,
-                        recording_player: call.recording_player,
-                        referrer_domain: call.referrer_domain,
-                        referring_url: call.referring_url,
-                        resource_id: call.id,
-                        source: call.source,
-                        source_name: call.source_name,
-                        start_time: call.start_time.slice(0,-6),
-                        total_calls: call.total_calls,
-                        tracker_resource_id: call.tracker_id,
-                        tracking_phone_number: call.tracking_phone_number,
-                        transcription: call.transcription,
-                        utm_campaign: call.utm_campaign,
-                        utm_content: call.utm_content,
-                        utm_medium: call.utm_medium,
-                        utm_source: call.utm_source,
-                        utm_term: call.utm_term,
-                        utma: call.utma,
-                        utmb: call.utmb,
-                        utmc: call.utmc,
-                        utmv: call.utmv,
-                        utmz: call.utmz,
-                        value: call.value,
-                        voicemail: call.voicemail,
-                        raw_webhook: JSON.stringify(call)
-                    });
-                };
-
-                // End the loop if it is the last page
-                if(results.data.calls.length < 250){
-                    break;
-                };
-
-                // Increment the page counter
-                page++;
-            };
-        // Save to BigQuery
-        await bigQuery
-            .dataset(bqDataset)
-            .table(bgBiggieCallsTable)
-            .insert(allCalls);
-        // Send success message
-        console.log(`${allCalls.length} calls from CallRail backlogged into BigQuery`);
-        res.status(200).send();
-    } catch (e) {
-        console.log(`CallRail backfill failed`);
-        console.log(e);
-        console.log(JSON.stringify(e));
-        res.status(500).send();
-    }
+            // Save to BigQuery
+            await bigQuery
+                .dataset(bqDataset)
+                .table(bgBiggieCallsTable)
+                .insert(allCalls);
+            // Send success message
+            console.log(`${allCalls.length} calls from CallRail backlogged into BigQuery`);
+            res.status(200).send();
+        } catch (e) {
+            console.log(`CallRail backfill failed`);
+            console.log(e);
+            console.log(JSON.stringify(e));
+            res.status(500).send();
+        }
     }
 }
